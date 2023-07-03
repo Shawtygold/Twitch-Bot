707f0d29b566c0968865e02cae07eea9f5145ddd
Shawtygold <shawtygoldz@gmail.com>
2023-07-03 20:54:39 +0500

    Добавил класс NavigationService и отсортировал директивы using

--- TwitchBot/Core/ViewModel.cs
+++ TwitchBot/Core/ViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwitchBot.Core
+﻿namespace TwitchBot.Core
 {
-    internal class ViewModel : ObservableObject
+    public class ViewModel : ObservableObject
     {
     }
 }
